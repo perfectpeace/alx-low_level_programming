94df8182a4a3c00d722fc1be61e5ee338d1c23ea
perfectpeace <perfectsunday428@gmail.com>
2022-03-10 17:22:05 -0800

    5-printf.c commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #!/bin/bash
 #include <stdio.h>
 
+/**
+ * main-Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece ot art,\n");
