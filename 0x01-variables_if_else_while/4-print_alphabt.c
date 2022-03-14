d750a32642035255c1850bd458343068d3987150
perfectpeace <perfectsunday428@gmail.com>
2022-03-13 21:36:19 -0700

    4-print_alphabt.c commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	if (ch == 'q')
+	}
+	continue;
+	{
+	if (ch == 'e')
+	}
+	continue;
+	{
+	putchar(ch);
+	}
+	putchar(10);
+	return (0);
+}
+
