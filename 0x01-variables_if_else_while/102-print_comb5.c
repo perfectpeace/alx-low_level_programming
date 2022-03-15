330f6eae21200544bcf6aadbe21298d702fd9f3f
perfectpeace <perfectsunday428@gmail.com>
2022-03-14 17:16:30 -0700

    102-print_comb5.c commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int k, l;
+
+	for (k = 0; k < 100; k++)
+	{
+		for (l = 0; l < 100; l++)
+		{
+			if (k < l)
+			{
+				putchar((k / 10) + 48);
+				putchar((k % 10) + 48);
+				putchar(' ');
+				putchar((l / 10) + 48);
+				putchar((l % 10) + 48);
+				if (k != 98 || l != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
