09b0fc5ff1909eba38ce6f7d552c443228985802
perfectpeace <perfectsunday428@gmail.com>
2022-03-16 22:42:07 -0700

    101-natural.c commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x, y;
+
+for (x = 1; x < 1024; x++)
+{
+if ((x % 3) == 0 || (x % 5) == 0)
+y += x;
+}
+printf("%d\n", y);
+return (0);
+}
+
