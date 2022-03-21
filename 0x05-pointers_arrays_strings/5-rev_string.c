51fbea35d967be1dcb695808a21ce64f4a7055a3
perfectpeace <perfectsunday428@gmail.com>
2022-03-21 15:29:39 -0700

    5-rev_string.c commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *rev_string - update value.
+ *@s: value to be evaluate.
+ *Return: not.
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char c;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	j = i - 1;
+	i = 0;
+	while (i < j)
+	{
+		c = s[j];
+		s[j] = s[i];
+		s[i] = c;
+
+		i++;
+		j--;
+	}
+}
