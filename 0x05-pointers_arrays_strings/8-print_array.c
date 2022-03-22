33069e6e3009772c679e236c9e6ef20ad3916cdf
perfectpeace <perfectsunday428@gmail.com>
2022-03-21 20:34:23 -0700

    8-print_array.c commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_array - function that prints an array.
+ *@a: int variable pointer
+ *@n: int type variable.
+ *Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n;  i++)
+	{
+		printf("%d", *(a + i));
+
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
