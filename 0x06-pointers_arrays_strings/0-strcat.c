3c99a9b0dbb524cc43196a0fe3844be4331ba61e
perfectpeace <perfectsunday428@gmail.com>
2022-03-24 19:21:14 -0700

    my initial commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,19 +5,26 @@
  * @dest: input value
  * @src: input value
  *
- * Return: void
+ * Return: Returns a pointer to the resulting string dest
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int a, b;
+	int i;
+	int j;
 
-	for (a = 0; dest[a] != '\0'; a++)
-		;
-		for (b = 0; src[b] != '\0'; b++)
-
-			dest[a + b] = src[b];
-
-		dest[a + b] = '\0';
-
-		return (dest);
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
