fce02b4f175ffd4830060fd9ba71ab720d2071ed
perfectpeace <perfectsunday428@gmail.com>
2022-04-02 13:25:31 -0700

    my commit

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int  a, b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+		;
+	for (b = 0; src[b] != '\0'; b++)
+
+		dest[a + b] = src[b];
+
+	dest[a + b] = '\0';
+
+	return (dest);
+}
