2b0b022c2341e814ae4aef25e46f773cfe3223cb
perfectpeace <perfectsunday428@gmail.com>
2022-04-06 22:01:10 -0700

    final commit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,6 +7,7 @@
  * @av: pointer to arguements
  * Return: pointer to new space in memory or null
  */
+
 char *argstostr(int ac, char **av)
 {
 	char *strDup;
@@ -15,7 +16,6 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	size = 0;
-/* count the number of chars in each string */
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
@@ -23,10 +23,6 @@ char *argstostr(int ac, char **av)
 		size++;
 	}
 	size++;
-/**
- * allocate memory for total number of chars and
- * new line for each word
- */
 	strDup = malloc(sizeof(char) * size);
 	if (strDup == NULL)
 		return (NULL);
